5c893c6cec5b4b0a44dff5d28652aca6071bd0f9
= <terrahen@web.de>
2017-09-22 21:31:42 +0200

    cli io

--- src/io/CommandIO.js
+++ src/io/CommandIO.js
@@ -7,6 +7,20 @@ module.exports = class CommandIO {
 
   constructor() {
     this._quiet = false;
+    this._base = null;
+  }
+
+  _ensurePath(path, appendBase = true) {
+    if (Array.isArray(path)) {
+      if (appendBase) {
+        path.unshift(this.getBase());
+      }
+      return Path.join.apply(Path, path);
+    }
+    if (appendBase) {
+      return Path.join(this.getBase(), path);
+    }
+    return Path.normalize(path);
   }
 
   quiet(quiet = true) {
@@ -18,7 +32,19 @@ module.exports = class CommandIO {
     return this._quiet;
   }
 
-  newline(count = 1) {
+  base(path = null) {
+    this._base = Path.normalize(path);
+    return this;
+  }
+
+  getBase() {
+    if (this._base === null) {
+      return boot.setting('root');
+    }
+    return this._base;
+  }
+
+  nl(count = 1) {
     if (this.isQuiet()) return this;
     for (let i = 0; i < count; i++) {
       this.out();
@@ -32,30 +58,65 @@ module.exports = class CommandIO {
     return this;
   }
 
+  h1(text) {
+    if (this.isQuiet()) return this;
+    this.nl().out('-', text).nl();
+    return this;
+  }
+
+  h2(text) {
+    if (this.isQuiet()) return this;
+    this.out('--', text).nl();
+    return this;
+  }
+
   error() {
     if (this.isQuiet()) return this;
     console.error.apply(console, arguments);
     return this;
   }
 
-  fsJson(path, value, always = false) {
+  fsMkDirs(path) {
+    path = this._ensurePath(path, false);
+    const parts = path.split(Path.sep);
+
+    let p = this.getBase();
+    for (const index in parts) {
+      p = Path.join(p, parts[index]);
+      if (!FS.existsSync(p)) {
+        FS.mkdirSync(p);
+        this.out('[FS] mkdir ' + this.subpath(p));
+      }
+    }
+  }
+
+  fsJson(path, value, overwrite = false) {
     path = this._ensurePath(path);
-    if (!FS.existsSync(path) || always) {
+    if (overwrite || !FS.existsSync(path)) {
       FS.writeFileSync(path, JSON.stringify(value, null, 2));
-      this.out('[FS] write json ' + this._subpath(path));
+      this.out('[FS] write json ' + this.subpath(path));
     }
     return this;
   }
 
-  _ensurePath(path) {
-    if (Array.isArray(path)) {
-      return Path.join.apply(Path, path);
+  fsCopy(from, to, overwrite = false) {
+    from = this._ensurePath(from);
+    to = this._ensurePath(to);
+
+    if (overwrite || !FS.existsSync(from)) {
+      FS.createReadStream(from).pipe(FS.createWriteStream(to));
+      this.out('[FS] copy from ' + this.subpath(from) + ' to ' + this.subpath(to));
     }
-    return Path.normalize(path);
+    return this;
   }
 
-  _subpath(path, rootPath = null) {
-    rootPath = rootPath || boot.setting('root');
+  rel(path) {
+    path = Path.normalize(this._ensurePath(path, false));
+    return path.substring(Path.normalize(this.getBase()).length + 1);
+  }
+
+  subpath(path, rootPath = null) {
+    rootPath = rootPath || this.getBase();
     return '"..' + path.substring(rootPath.length) + '"';
   }
 
